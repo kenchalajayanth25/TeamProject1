850543afd7720bb5af3a131f2c53466277f0ff29
kench <kench@jayanth>
2022-03-02 10:16:27 +0530

    team view

---
+++ src/Teamprj.java
@@ -0,0 +1,16 @@
+
+public class Teamprj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int age=18;
+if (age==18){
+	System.out.println("applicable");
+}
+else
+{
+	System.out.println("not applicabel");
+}
+	}
+
+}
